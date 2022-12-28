5cb6c6f10da726ac70783b32715ebf8465316fbb
Oyewole Oni <onioyewole1910@gmail.com>
2022-12-28 17:23:12 +0100

    Adding appStyles file

---
+++ lib/app_styles.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+//Colors
+const Color kPageBackgroundColor =  Color(0xFF262626);
+const Color kDarkColor =  Color(0xFF2F2F2F);
+const Color kLightColor =  Color(0xFFF5F5F5);
+const Color kDarkTextColor =  Color(0xFF151515);
+const Color kLightTextColor =  Color(0xFFF5F5F5);
+const Color kLightBorderColor =  Color(0xFFA5A5A5);
+const Color kCardFillColor =  Color(0xFF3D3D3D);
+
+
+//Text Styles
+
+const TextStyle kRegularText12 = TextStyle(fontFamily: "RegularFont", color: kLightColor, fontSize: 12);
