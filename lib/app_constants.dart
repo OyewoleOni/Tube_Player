2743b9ec284309f99e7a7e0d970664b5fdfde069
Oyewole Oni <onioyewole1910@gmail.com>
2022-12-28 03:29:37 +0100

    Adding constant file

---
+++ lib/app_constants.dart
@@ -0,0 +1,6 @@
+class Constants{
+  static const String ApplicationName= "Tube Playe";
+  static const String EmailAddress= "onioyewole1910@gmail.com";
+  static const String ApiServiceURL= "https://youtube.googleapis.com/youtube/v3/";
+  static const String ApiKey= "AIzaSyBJFIP_f_SfkMfOSv4y5NgS77fZFOr91ec";
+}
